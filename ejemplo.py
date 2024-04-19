e52e9181e3ec3a53ee06bd52b51f39408500b749
EclipseLunarw <167488362+EclipseLunarw@users.noreply.github.com>
2024-04-18 23:29:55 -0400

    Delete ejemplo.py

--- ejemplo.py
+++
@@ -1,9 +0,0 @@
-#Pregunta 1
-#Crear un script en Python que imprima el texto “Evaluación N°1 Programación y Redes Virtualizadas” y su nombre y apellido
-
-nombres= "Michel_Alejandro"
-apellidos= "Matus_Morales"
-ev1="Evaluación N°1 Programación y Redes Virtualizadas"
-print(ev1)
-print(nombres)
-print(apellidos)
