d7f96e9de4dcbfccbe1269d5edf579fd707ec20f
EclipseLunarw <mic.matus@duocuc.cl>
2024-04-18 20:51:12 -0400

    Archivo-Ev1

---
+++ 2.py
@@ -0,0 +1,12 @@
+#Pregunta 2
+#Crear un script en Python que solicite información como el nombre, apellido, Código-sección y sede. La información debe aparecer en pantalla.
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+seccion = input("Ingrese su numero de sección: ")
+sede = input("Ingrese su sede: ")
+
+print("Información ingresada")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Código de sección:", seccion)
+print("Sede:", sede)
